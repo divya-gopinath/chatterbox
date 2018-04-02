faf2fe9e34eaf7d90f074001716d6aa552a54952
Divya Gopinath <divyagop@mit.edu>
2018-04-02 16:49:47 -0400

    Updated emoji bank

--- emoji_bank.py
+++ emoji_bank.py
@@ -1,10 +1,10 @@
 emoji_bank = {
     "happiness" : [":slightly_smiling_face:", ":smiling_face_with_smiling_eyes:", ":grinning_face_with_smiling_eyes:", ":grinning_face:"],
-    "sadness" : ["U+1F641", "U+1F61F", "U+1F62D"],
-    "disgust" : ["U+1F62C", "U+1F92E"],
-    "anger" : ["U+1F620", "U+1F621"],
-    "contempt" : ["U+1F612", "U+1F644", "U+1F611"],
-    "fear" : ["U+1F628", "U+1F627", "U+1F626"],
-    "surprise" : ["U+1F632", "U+1F633"],
+    "sadness" : [":slightly_frowning_face:", ":worried_face:", ":loudly_crying_face:"],
+    "disgust" : [":grimacing_face:", ":face_vomiting:", ":nauseated_face:"],
+    "anger" : [":angry_face:", ":pouting_face:"],
+    "contempt" : [":unamused_face:", ":face_with_rolling_eyes:", ":expressionless_face:"],
+    "fear" : [":fearful_face:", ":anguished_face:", ":frowning_face_with_mouth_open:"],
+    "surprise" : [":astonished_face:", ":flushed_face:"],
     "neutral" : None,
 }
