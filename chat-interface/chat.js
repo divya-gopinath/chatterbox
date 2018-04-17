eaf19e148ab12683345225edb40e89d0200b3045
Mary Thielking <mthielki@mit.edu>
2018-04-17 16:08:11 -0400

    Signin with username

--- chat-interface/chat.js
+++ chat-interface/chat.js
@@ -1,4 +1,5 @@
-var USERNAME = "you";
+var USERNAME = ""; // gets set when user signs in
+var CHATTING = false; // switches to true when user signs in
 
 // Holds DOM elements that don’t change, to avoid repeatedly querying the DOM
 var dom = {};
@@ -8,13 +9,27 @@ document.addEventListener("DOMContentLoaded", function() {
   dom.msgs = document.querySelector("#sent-msgs");
   dom.input = document.querySelector("#msg-input");
   dom.send = document.querySelector("#send-btn");
+  dom.signin = document.querySelector("#signin-btn");
+  dom.name = document.querySelector("#name-input");
+  dom.popup = document.querySelector("#popup");
 
   dom.send.addEventListener("click", send);
+  dom.signin.addEventListener("click", signin);
   dom.input.addEventListener("keydown", function(evt) {
     if (evt.key === "Enter") { send(); }
   });
-  dom.input.focus();
+  dom.name.addEventListener("keydown", function(evt) {
+    if (evt.key === "Enter") { signin(); }
+  });
+  dom.name.focus();
 });
+
+function signin() {
+  USERNAME = dom.name.value;
+  CHATTING = true;
+  dom.popup.style.setProperty("display", "none");
+  dom.input.focus();
+}
 
 function send() {
   createMsgDiv(USERNAME, dom.input.value);
